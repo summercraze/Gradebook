bc32117c3744c3fbdbf40f6eb5b4708958476a13
Rong Huang <rhuang92@gmail.com>
2022-07-26 20:26:04 -0700

    Commit initial project to github

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,14 @@
+using System.Globalization;
+namespace GradeBook
+{
+    class Book
+    {
+        List<double> grades = new List<double>()
+
+         public addGrade(double grade)
+        { 
+           grades.Add(grade);
+        }
+
+    }
+}
