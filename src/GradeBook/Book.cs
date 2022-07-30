51726af01eaa1e25d25a1719420d808f8b1da258
Rong Huang <rhuang92@gmail.com>
2022-07-29 23:06:32 -0700

    added new statistic calculator to code

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,14 +1,54 @@
-using System.Globalization;
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
-        List<double> grades = new List<double>()
+        private List<double> grades;
 
-         public addGrade(double grade)
-        { 
-           grades.Add(grade);
+        private string name;
+
+        private static double
+
+                highestGrade,
+                lowestGrade,
+                average,
+                total;
+
+        public Book(String name)
+        {
+            grades = new List<double>();
+            this.name = name;
         }
 
+        public void addGrade(double grade)
+        {
+            grades.Add (grade);
+        }
+
+        public void statistic()
+        {
+            highestGrade = double.MinValue;
+            lowestGrade = double.MaxValue;
+            total = 0;
+            average = 0;
+            foreach (var num in grades)
+            {
+                if (num > highestGrade)
+                {
+                    highestGrade = num;
+                }
+                if (num < lowestGrade)
+                {
+                    lowestGrade = num;
+                }
+                total += num;
+            }
+            average = total / grades.Count;
+            Console.WriteLine($"The highest grade is {highestGrade:N1}");
+            Console.WriteLine($"The lowest grade is {lowestGrade:N1}");
+            Console.WriteLine($"The average grade is {average:N1}");
+        }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,39 +1,19 @@
-﻿using System.Reflection.Metadata;
-using System.Threading.Tasks.Dataflow;
-using System.Globalization;
-using System;
+﻿using System;
 using System.Collections.Generic;
+
 namespace GradeBook
 {
-    
     class Program
     {
         static void Main(string[] args)
         {
-            var book = new Book();
-            // var numbers = new [] {35.7, 67.8, 12.3};
+            var book = new Book("Rong's book");
             book.addGrade(12.0);
-            double sum = 0;
-            // for(int i = 0; i < numbers.Length; i++)
-            // {
-            //    sum += numbers[i];
-            // }
-          
-           List<double> grades = new List<double>(){35.7, 67.8, 12.3};
-           grades.Add(56.1);
-           foreach(var number in grades)
-           {
-            sum +=number;
-           }
-           double average = sum/grades.Count;
-             Console.WriteLine("The sum of array is : " + sum);
-               Console.WriteLine($"The average of array is {average:N2}");
-            // double x = 35.7; 
-            // double y = 67.8;
-            // double z = x + y;
-            // Console.WriteLine("The sum of " + x + " and " + y + " is " + z);
-
-           
+            book.addGrade(14.0);
+            book.addGrade(16.0);
+            book.addGrade(18.0);
+            book.addGrade(20.0);
+            book.statistic();
         }
     }
 }
